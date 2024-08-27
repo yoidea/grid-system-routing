cd8ee1cda7e31d429f13eef0d5a1a4e8809e73fc
yoidea <yoidea.cc@gmail.com>
2024-08-28 01:37:53 +0900

    ダイエットコーク

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,6 +1,4 @@
 import Image from 'next/image'
-import streetsData from './../../data/streets.yaml'
-import avenuesData from './../../data/avenues.yaml'
 
 
 export const dynamic = 'force-dynamic'
@@ -10,41 +8,21 @@ interface Params {
 }
 
 export default async function Home({ params }: { params: Params }) {
-  const streets = streetsData.streets
-  const avenues = avenuesData.avenues
-
-  const getNormalizedAddress = (address: string) => {
-    const grid = address.split(/上ル|下ル|東入ル|西入ル/, 1)[0].split("通")
-    const matchResult = address.match(/上ル|下ル|東入ル|西入ル/)
-    if (matchResult === null) return null
-    const suffix = matchResult[0]
-    const street = streets.includes(grid[0]) ? grid[0] : streets.includes(grid[1]) ? grid[1] : null
-    const avenue = avenues.includes(grid[0]) ? grid[0] : avenues.includes(grid[1]) ? grid[1] : null
-    if (street === null || avenue === null) return null
-    if (suffix === "上ル") {
-      return `${avenue}通${street}上ル`
-    }
-    if (suffix === "下ル") {
-      const streetIndex = streets.findIndex((name: string) => name === street)
-      return `${avenue}通${streets[streetIndex + 1]}上ル`
-    }
-    if (suffix === "東入ル") {
-      return `${street}通${avenue}東入ル`
-    }
-    if (suffix === "西入ル") {
-      const avenueIndex = avenues.findIndex((name: string) => name === avenue)
-      return `${street}通${avenues[avenueIndex + 1]}東入ル`
-    }
-  }
+  const convert2HS = (n: string) => n.replace(/[０-９]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 65248))
 
   const address = decodeURIComponent(params.slug)
-  const normalizedAddress = getNormalizedAddress(address)
+  const grid = address.split("丁目", 1)[0].split(/条|大通/)
+  
+  const matchResult = address.match("丁目")
+  if (matchResult === null) return null
+  const x = grid[0] === "" ? 0 : grid[0][0] === "北" ? convert2HS(grid[0][1]) : -convert2HS(grid[0][1])
+  const y = grid[1][0] === "東" ? convert2HS(grid[1][1]) : -convert2HS(grid[1][1])
 
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       <h1>{decodeURIComponent(params.slug)}</h1>
       <Image 
-        src={`/${normalizedAddress}.jpg`} 
+        src={`/${x},${y}.jpg`} 
         alt="サンプル画像" 
         width={150}
         height={150}
